3f1d6498be84000678a73eda9213dadfe0a43ccd
mahdi dortaj <mahdidortaj@mahdis-MacBook-Pro.local>
2020-08-23 02:08:43 +0430

    creating db models, need to be completed

---
+++ db/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from views import app
+
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+    person_id = db.Column(db.Integer, primary_key=True)
+    lname = db.Column(db.String(32))
+    fname = db.Column(db.String(32))
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
