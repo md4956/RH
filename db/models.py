2a2c5faa2508fe6418ef6e413722d9d7324f65ca
mahdi d <md4956@yahoo.com>
2020-08-24 01:52:39 +0430

    working on db models and related funcs

--- db/models.py
+++ db/models.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from views import app
+from run import app
 
 db = SQLAlchemy(app)
 
 
-class Person(db.Model):
-    __tablename__ = "person"
-    person_id = db.Column(db.Integer, primary_key=True)
-    lname = db.Column(db.String(32))
-    fname = db.Column(db.String(32))
-    timestamp = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
-    )
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+def create_new_user(username, password, email):
+    new_user = User(username=username, email=email, password=password)
+    db.session.add(new_user)
+    print('new user "'+username+'" has been created.')
--- views.py
+++ views.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
+from flask import render_template, request, redirect, url_for
+from run import app
+from db import models as dbModel
+from db.models import db
 
 
 @app.route('/')
 def home():
+    db.create_all()
     return redirect(url_for('login'))
 
 
@@ -23,8 +25,9 @@ def login():
 def signup():
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['pass']
-        print(username, password)
+        dbModel.create_new_user(username, password, email)
         return redirect(url_for('login'))
     else:
         return render_template('signup/signup.html')
